229668988697b64f67659891b44f7ce491d210e1
Ujjwal Parashar <whomtalkto@gmail.com>
2025-01-12 01:20:31 +0530

    updated package.json

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://spontaneous-cobbler-2978a6.netlify.app/",
+    origin: "https://spontaneous-cobbler-2978a6.netlify.app",
   },
 });
 
